d1d95b05f2f5619c1957602db6f777fe049c0241
toheeb-orelope <toheeborelope@gmail.com>
2026-04-29 17:19:55 +0100

    format terraform files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,12 +1,12 @@
 # Link to backend documentation: https://registry.terraform.io/modules/nozaq/remote-state-s3-backend/aws/latest
 terraform {
   backend "s3" {
-    bucket         = "project-363238514491-us-east-1-an"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    kms_key_id     = "eee61541-778a-4cd7-a0c4-b14457fffe4d"
-    # use_lockfile = true
-    dynamodb_table = "dynamodb-table1"
+    bucket     = "project-363238514491-us-east-1-an"
+    key        = "envs/dev/terraform.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
+    kms_key_id = "eee61541-778a-4cd7-a0c4-b14457fffe4d"
+    use_lockfile = true
+    # dynamodb_table = "dynamodb-table1"
   }
 }
