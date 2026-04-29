82f53f0b666f7721ae7baaad16a338a76ff37b3c
toheeb-orelope <toheeborelope@gmail.com>
2026-04-29 18:14:44 +0100

    remove deprecated dynamodb state locking

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,12 @@
 # Link to backend documentation: https://registry.terraform.io/modules/nozaq/remote-state-s3-backend/aws/latest
 terraform {
   backend "s3" {
-    bucket         = "project-363238514491-us-east-1-an"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    kms_key_id     = "b2948092-a0d4-4310-b838-316e430bae1f"
-    dynamodb_table = "dynamodb-table1"
+    bucket     = "project-363238514491-us-east-1-an"
+    key        = "envs/dev/terraform.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
+    kms_key_id = "b2948092-a0d4-4310-b838-316e430bae1f"
+    # dynamodb_table = "dynamodb-table1"
+    use_lockfile = true
   }
 }
